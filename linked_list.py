cbf39e8fc83fc163da2381ffd509b933741a2ffd
HuyCan1605 <95010802+HuyCan1605@users.noreply.github.com>
2024-06-03 22:41:35 +0700

    add function INSERT, REMOVE, SEARCH

--- linked_list.py
+++ linked_list.py
@@ -33,6 +33,63 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
+    def search(self, key):
+        """
+        SEarch for the first node contraining data that matches the key
+        return the node or 'None' if not found
+        """
+        current = self.head
+
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        
+        return None
+
+    def insert(self, data, index):
+        if index == 0:
+            self.add(data)
+        
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next_node
+                position -= 1
+
+            prev = current
+            next_node = current.next_node
+
+            prev.next_node = new 
+            new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes node containing data that matches the key 
+        Returns the node or none if key doesn't exist
+        Takes O(n) time
+        """
+        
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current == self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
     def __repr__(self):
         """Return a string representation of the list take 0(n) time"""
         nodes = []
